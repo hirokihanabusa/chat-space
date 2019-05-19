71722b7e16694ba76ffdb176f5c54bd870952551
hirokihanabusa <clouscolsunset@yahoo.co.jp>
2019-05-19 18:37:56 +0900

    Edit_users_controller.rb
    Whereメソッド記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @user = user.find(params[:id])
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
     respond_to do |format|
       format.html
       format.json
