b30601abe74341ad63a6b2c6f12316247b80ac2b
hirokihanabusa <clouscolsunset@yahoo.co.jp>
2019-05-19 18:35:13 +0900

    Edit_user.js
    インクリメンタルサーチの実装

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,5 +1,42 @@
 $(function() {
-  $(".search__query").on("keyup", function() {
-    var input = $(".search__query").val();
+
+  var search_list = $("#user-search-result");
+
+  function appendUser(user) {
+    var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user.name}</p>
+                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>`
+    search_list.append(html);
+  }
+
+  function appendNoUser(user) {
+    var html = `<div class="chat-group-form__search clearfix">
+                </div>`
+    search_list.append(html);
+  }
+
+  $("#user-search-field").on("keyup", function(e) {
+    var input = $("#user-search-field").val();
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+    .done(function(users) {
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else {
+        appendNoUser("一致するユーザーが見つかりません");
+      }
+    })
+    .fail(function() {
+      alert('ユーザー検索に失敗しました');
+    });
   });
 });
