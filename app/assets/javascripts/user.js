6c69dbe95844c7f0c6c84b9e81d890e14a964b3c
hirokihanabusa <clouscolsunset@yahoo.co.jp>
2019-06-08 19:12:44 +0900

    訂正
    コンフリクト解消

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -10,6 +10,7 @@ $(document).on('turbolinks:load', function() {
                   <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
                 </div>`
     search_list.append(html);
+    return html;
   }
 
   function appendNoUser(user) {
