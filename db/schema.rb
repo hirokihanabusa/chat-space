0599e14dd0225de51b4c987b8cbce07b4d397c8c
hirokihanabusa <clouscolsunset@yahoo.co.jp>
2019-06-12 20:50:20 +0900

    initial_commit

--- db/schema.rb
+++ db/schema.rb
@@ -22,9 +22,9 @@ ActiveRecord::Schema.define(version: 20190428043445) do
   end
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "name"
   end
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 20190428043445) do
     t.string   "image"
     t.integer  "group_id"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
     t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
